442e5db2eae5945f90839190f93fcc98474f4b5e
camilamejiax <96745732+camilamejiax@users.noreply.github.com>
2022-04-26 18:43:53 -0400

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header img-fluid">
+      <div className="container">
+        <header className="App-header img-fluid">
           <img src={logo} className="App-logo" alt="logo" />
-      </header>
+        </header>
+      
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>Coded by Camila Mejia</small>
+        </footer>
+      </div>
     </div>
   );
 }
